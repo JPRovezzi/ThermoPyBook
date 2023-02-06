0b0b094e56e09ad971646e033ad195491bede671
Juan Pablo Rovezzi <juanrovezzi@hotmail.com>
2023-02-06 14:55:52 -0300

    Primeros pasos

---
+++ 1st2ndLaw.py
@@ -0,0 +1,10 @@
+class Concept:
+    what = "Oh, this field seems to be empty."
+
+energy = Concept()
+google = Concept()
+
+energy.what = "Enegy is the sum total of all capacity"
+print(energy.what)
+print(google.what)
+
